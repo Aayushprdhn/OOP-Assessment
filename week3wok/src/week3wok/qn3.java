102e2f4af186f18d026d41d505460398bd848949
Aayush <pradhanaayush289@gmail.com>
2026-03-23 18:54:38 +0545

    Updated ans

--- week3wok/src/week3wok/qn3.java
+++ week3wok/src/week3wok/qn3.java
@@ -5,6 +5,14 @@ class BankAccount{
 	double balance;
 	String accountHolderName;
 	String accountHolderAddress;
+	
+	void Forprint(){
+		System.out.println("Account Number: "+ accountNumber);
+		System.out.println("Balance: "+ balance);
+		System.out.println("Account Holder's Name: "+ accountHolderName);
+		System.out.println("Account Holder's Address: "+ accountHolderAddress);
+
+	}
 }
 
 public class qn3 {
---
+++ week3wok/src/week3wok/qn4.java
@@ -0,0 +1,13 @@
+package week3wok;
+
+public class qn4 {
+	public static void main(String[] args) {
+		
+		BankAccount acc = new BankAccount();
+		acc.accountNumber = 23110;
+		acc.balance = 4533.34;
+		acc.accountHolderName = "Aayush";
+		acc.accountHolderAddress = "Biratnagar 14";
+		acc.Forprint();
+	}
+}
