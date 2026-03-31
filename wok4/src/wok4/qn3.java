96f431fbea36233467b631185c488c44d935cb84
Aayush <pradhanaayush289@gmail.com>
2026-03-31 17:51:24 +0545

    upto no.3

---
+++ wok4/src/wok4/qn3.java
@@ -0,0 +1,44 @@
+
+package wok4;
+
+
+
+public class qn3 {
+
+	public static void main(String[] args) {
+		
+	
+		Dog2 dog2 = new Dog2("Haiti");
+	}
+}
+
+class Animal2{
+	
+	public String name;
+	public int age;
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	public int getAge() {
+		
+		return age;
+	}
+	
+	public Animal2(String name) {
+		this.name = name;
+		
+		System.out.println(name);
+	}
+}
+
+class Dog2 extends Animal2{
+	
+	public Dog2(String name) {
+		super(name);
+		this.name = name;
+	}
+}
+	
