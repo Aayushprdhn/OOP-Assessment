8d20bf2d57c827ec377048d78b3ad6cdfe5fcadd
Aayush <pradhanaayush289@gmail.com>
2026-04-14 01:16:47 +0545

    Completed wok6

---
+++ wok6/src/wok6/qn10.java
@@ -0,0 +1,13 @@
+package wok6;
+import java.util.*;
+public class qn10 {
+	public static void main(String[] args) {
+		
+		HashMap <String, Long> detail = new HashMap<>();
+		
+		detail.put("Aayush", 987123332L);
+		detail.put("Me", 98289121L);
+		
+		System.out.println(detail);
+	}
+}
---
+++ wok6/src/wok6/qn14.java
@@ -0,0 +1,14 @@
+package wok6;
+import java.util.*;
+public class qn14 {
+	public static void main(String[] args) {
+		
+		ArrayList <Integer> binary = new ArrayList<>(List.of());
+		
+		Collections.addAll(binary, 1,2,3,4,5,6,7,8,9,10);
+		
+		int result = Collections.binarySearch(binary,7);
+		
+		System.out.println("Index: "+result);
+	}
+}
