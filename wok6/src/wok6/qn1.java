4364fd250694e02ac19e84f56636d72df4f0ff58
Aayush <pradhanaayush289@gmail.com>
2026-04-13 22:23:20 +0545

    Starting week6

---
+++ wok6/src/wok6/qn1.java
@@ -0,0 +1,17 @@
+package wok6;
+import java.util.*;
+public class qn1 {
+	public static void main(String[] args) {
+		ArrayList <String> students = new ArrayList<>();
+		
+		students.add("Aayush");
+		students.add("Roshan");
+		students.add("Suraj");
+		students.add("Prithivi");
+		students.add("Balav");
+		
+		students.remove(2);
+		
+		System.out.println(students);
+	}
+}
