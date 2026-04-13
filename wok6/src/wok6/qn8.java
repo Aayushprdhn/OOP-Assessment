d73cdec33d5b89c9626b9cc35d0cf94b94346a07
Aayush <pradhanaayush289@gmail.com>
2026-04-13 23:35:36 +0545

    upto 9

---
+++ wok6/src/wok6/qn8.java
@@ -0,0 +1,14 @@
+package wok6;
+import java.util.*;
+public class qn8 {
+	public static void main(String[] args) {
+		
+		TreeSet<String> name = new TreeSet<>();
+		
+		name.add("Aayush");
+		name.add("Kushal");
+		name.add("Aaditya");
+		
+		System.out.println(name);
+	}
+}
